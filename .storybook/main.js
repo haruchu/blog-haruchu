c789d97575b7045dc4c94d18282def82ab56cedf
haru <haruki6180@gmail.com>
2022-04-15 13:57:09 +0900

    storybook導入

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/**/story.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
