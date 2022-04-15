ab35bd84f43a95e451bd3bbe984a385082eb090a
haru <haruki6180@gmail.com>
2022-04-15 14:14:40 +0900

    storybook 動作確認

---
+++ src/text/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Example: React.FC = () => {
+  return <text>example.</text>;
+};
+export default Example;
---
+++ src/text/story.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Component from "./index";
+
+export default {
+  component: Component,
+  title: "Example",
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
+
+export const Basic = () => {
+
+  return (
+    <Component/>
+  );
+};
