1cb65546a41a118cab9500730a796b1f208f7e4d
haru <haruki6180@gmail.com>
2022-04-19 15:49:06 +0900

    メイン色の定義

--- src/components/valiables/Color.ts
+++ src/components/valiables/Color.ts
@@ -1,4 +1,12 @@
 const COLOR: string[] = [
   "#40DFEF", "#B9F8D3", "#E78EA9", "#F6FFA4", "#CE49BF", "#65C18C", "#FFF8F3"
 ];
-export default COLOR;
+
+const MAIN_COLOR = {
+  "WHITE_BLUE": "#EFFFFD",
+  "LIGHT_BLUE": "#B8FFF9",
+  "NOMAL_BLUE": "#85F4FF",
+  "DARK_BLUE": "#42C2FF"
+};
+
+export default {COLOR, MAIN_COLOR};
