af5496faf2b8cee239246b24bbb59cc0219932fa
haru <haruki6180@gmail.com>
2022-04-19 15:57:56 +0900

    色の定義修正

--- src/components/valiables/Color.ts
+++ src/components/valiables/Color.ts
@@ -1,12 +1,10 @@
-const COLOR: string[] = [
+export const COLOR = [
   "#40DFEF", "#B9F8D3", "#E78EA9", "#F6FFA4", "#CE49BF", "#65C18C", "#FFF8F3"
 ];
 
-const MAIN_COLOR = {
+export const MAIN_COLOR = {
   "WHITE_BLUE": "#EFFFFD",
   "LIGHT_BLUE": "#B8FFF9",
   "NOMAL_BLUE": "#85F4FF",
   "DARK_BLUE": "#42C2FF"
 };
-
-export default {COLOR, MAIN_COLOR};
