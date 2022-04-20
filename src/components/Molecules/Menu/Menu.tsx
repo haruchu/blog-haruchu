a18baf827e80152f2c49871788a2f944e834259c
haru <haruki6180@gmail.com>
2022-04-20 12:24:48 +0900

    メニュー定義

---
+++ src/components/Molecules/Menu/Menu.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { AiOutlineHome } from "react-icons/ai"
+import { MdOutlineArticle } from "react-icons/md"
+import { BsCalendarWeek } from "react-icons/bs"
+
+export const Menu = [
+  {
+    "name": "HOME",
+    "icon": <AiOutlineHome/>
+  },
+  {
+    "name": "articles",
+    "icon": <MdOutlineArticle/>
+  },
+  {
+    "name": "calendar",
+    "icon": <BsCalendarWeek/>
+  }
+]
