d2ee5cb2cef3c6be567fdeedafee23aa855418b7
haru <haruki6180@gmail.com>
2022-04-22 14:44:30 +0900

    おおかた完成

---
+++ src/components/atoms/MenuList/BottomMenuList.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import BottomMenuList from "./BottomMenuList";
+import { AiOutlineHome } from "react-icons/ai";
+
+export default {
+  component: BottomMenuList,
+  title: "Atoms/BottomMenuList",
+  parameters: {
+    backgrounds: {
+      default: "light",
+    },
+  },
+};
+
+export const isActive = () => {
+  const onActive = () => {
+    console.log("Active");
+  }
+
+  return (
+    <BottomMenuList listName={"Home"} index={1} openIndex={1} onActive={onActive}>
+      <AiOutlineHome />
+    </BottomMenuList>
+  );
+};
+
+export const notActive = () => {
+  const onActive = () => {
+    console.log("Active");
+  }
+
+  return (
+    <BottomMenuList listName={"Home"} index={1} openIndex={2} onActive={onActive}>
+      <AiOutlineHome />
+    </BottomMenuList>
+  );
+};
---
+++ src/components/atoms/MenuList/BottomMenuList.tsx
@@ -0,0 +1,87 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+export interface MenuListProps {
+  listName: string;
+  children: ReactNode;
+  index: number;
+  openIndex: number;
+  onActive: React.MouseEventHandler<HTMLLIElement>;
+}
+
+const BottomMenuList: React.FC<MenuListProps> = ({ listName, children, index, openIndex, onActive }) => {
+  return (
+    <StyledList
+      className={openIndex == index ? 'list active' : 'list'}
+      index={index}
+      isOpen={index == openIndex ? true : false}
+      onClick={onActive}
+    >
+      <StyledLink href='#'>
+        <StyledIcon className='icon'>
+          {children}
+        </StyledIcon>
+        <StyledText className='text'>
+          {listName}
+        </StyledText>
+      </StyledLink>
+    </StyledList>
+
+  );
+};
+export default BottomMenuList;
+
+
+const StyledList = styled.li<{ index: number; isOpen: boolean}>`
+  list-style: none;
+  width: 70px;
+  height: 70px;
+  z-index: 1;
+
+  ${(props) => `
+    ${props.isOpen && `
+      &.active a .icon {
+        transform: translateY(-35px);
+      }
+
+      &.active a .text {
+        opacity: 1;
+        transform: translateY(10px);
+      }
+    `}
+    &:nth-child(${props.index}).active ~ .indicator
+    {
+      transform: translateX(calc(70px * ${props.index}));
+    }
+  `}
+`
+
+const StyledLink = styled.a`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  font-weight: 500;
+`
+
+const StyledIcon = styled.span`
+  display: block;
+  line-height: 75px;
+  font-size: 28px;
+  text-align: center;
+  transition: 0.5s;
+  color: black;
+`
+
+const StyledText = styled.span`
+  position: absolute;
+  color: black;
+  font-weight: 400;
+  font-size: 16px;
+  transition: 0.5s;
+  opacity: 0;
+  transform: translateY(20px);
+`
