cc12494b5753b96e8bde166510d35d1c4732f9e3
haru <haruki6180@gmail.com>
2022-04-28 10:23:51 +0900

    方針大幅変更　githubの草みたいに

--- src/components/Molecules/Contributions/Contributions.tsx
+++ src/components/Molecules/Contributions/Contributions.tsx
@@ -1,34 +1,74 @@
 import React from "react";
-import Calendar from 'tui-calendar'; /* ES6 */
-import "tui-calendar/dist/tui-calendar.css";
-import 'tui-date-picker/dist/tui-date-picker.css';
-import 'tui-time-picker/dist/tui-time-picker.css';
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
+import ReactTooltip from "react-tooltip";
+import styled from "styled-components";
 
 const Contributions = () => {
-
-  const calendar = new Calendar('#calendar', {
-    defaultView: "month",
-    useCreationPopup: true,
-    useDetailPopup: true
-  });
+  const today = new Date(); // 変数が呼ばれた時の日時がDate型で取得できる
+  const year = today.getFullYear();
 
   return (
-    <>
-      <div id="menu">
-        <span id="menu-navi">
-          <button type="button" className="btn btn-default btn-sm move-today" data-action="move-today">Today</button>
-          <button type="button" className="btn btn-default btn-sm move-day" onClick={() => calendar.prev()}>
-            <i className="calendar-icon ic-arrow-line-left" data-action="move-prev">前</i>
-          </button>
-          <button type="button" className="btn btn-default btn-sm move-day" onClick={() => calendar.next()}>
-            <i className="calendar-icon ic-arrow-line-right" data-action="move-next">次</i>
-          </button>
-        </span>
-        <span id="renderRange" className="render-range"></span>
-      </div>
-
-      <div id="calendar" onClick={() => calendar.getDate()}></div>
-    </>
+    <Container>
+      <Year>
+        { year }
+      </Year>
+      <Calender>
+        <CalendarHeatmap
+          startDate={new Date(year + "-01-01")}
+          endDate={new Date(year + "-12-31")}
+          values={[
+            { date: "2022-07-03", count: 1 },
+            { date: "2022-08-22", count: 2 },
+            { date: "2022-07-29", count: 4 },
+            { date: '2022-10-01', count: 1 },
+            { date: '2022-10-03', count: 2 },
+            { date: '2022-10-06', count: 3 },
+            { date: '2022-10-10', count: 4 },
+            { date: '2022-10-07', count: 1 },
+            { date: '2022-09-15', count: 3 },
+          ]}
+          classForValue={(value) => {
+            if (!value) {
+              return "color-empty";
+            }
+            return `color-scale-${value.count}`;
+          }}
+          tooltipDataAttrs={(value: { date: Date; count: number; }) => {
+            if (!value || !value.date) {
+              return null;
+            }
+            return {
+              "data-tip": `${value.date} has count: ${
+                value.count
+              }`,
+            };
+          }}
+        />
+      </Calender>
+      <ReactTooltip />
+    </Container>
   );
 };
+
 export default Contributions;
+
+const Container = styled.div`
+  width: 90vw;
+  max-width: 1050px;
+  white-space: nowrap;
+  overflow-x: scroll;
+  border: solid 1px #696969;
+  position: relative;
+`
+
+const Year = styled.span`
+  position: fixed;
+  margin: 4px;
+`
+
+const Calender = styled.div`
+  display: inline-block;
+  width: 1000px;
+  margin: 30px;
+`
