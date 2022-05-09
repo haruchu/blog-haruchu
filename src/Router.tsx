68bfdd4542d6c6b9e91a21188b2486a15f9315b8
haru <haruki6180@gmail.com>
2022-05-09 16:42:58 +0900

    ルーティング

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { SampleHome } from "./components/SampleHome";
-import { SamplePage1 } from "./components/SamplePage1";
-import { SamplePage2 } from "./components/SamplePage2";
+import ArticlesPage from "pages/ArticlesPage";
+import ContributionsPage from "pages/ContributionsPage";
+import HomePage from "pages/HomePage";
 
 export const Router: React.VFC = () => {
   return (
     <Routes>
-      <Route index element={<SampleHome />} />
-      <Route path="page1" element={<SamplePage1 />} />
-      <Route path="page2" element={<SamplePage2 />} />
+      <Route index element={<HomePage/>} />
+      <Route path="articles" element={<ArticlesPage/>} />
+      <Route path="contributions" element={<ContributionsPage/>} />
     </Routes>
   );
 };
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomePage: React.FC = () => {
+
+  return (
+    <>HomePage</>
+  );
+};
+export default HomePage;
