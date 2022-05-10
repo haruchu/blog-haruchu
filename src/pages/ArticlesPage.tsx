01c874fdd49793b75c2c32350953495fae87335a
haru <haruki6180@gmail.com>
2022-05-10 12:52:52 +0900

    データ取得できた

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,18 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { useEffect } from 'react';
 import { db } from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import styled from "styled-components";
+import ArticleLink from "components/atoms/article/ArticleLink";
+
+
+type Articles = {
+  id: string;
+  title: string;
+  tags: string[];
+  date: string;
+}
 
 const ArticlesPage: React.FC = () => {
+  const [articles, setArticles] = useState<Articles[]>([]);
 
   useEffect(() => {
     const usersCollectionRef = collection(db, 'articles');
     getDocs(usersCollectionRef).then((querySnapshot) => {
-      querySnapshot.docs.forEach((doc) => console.log(doc.data()));
-    });  }, []);
+      const newArticles: any[] = [];
+      querySnapshot.forEach((doc) => {
+        newArticles.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setArticles(newArticles);
+    });
+    console.log(articles);
+  }, []);
 
-  return <>ArticlesPage</>;
+
+  return (
+    <>
+      {articles.map((article) => (
+        <div key={article.id}>{article.title}</div>
+      ))}
+    </>
+  );
 };
 export default ArticlesPage;
